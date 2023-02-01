aae6570e5bcb5ef5c71a0960240d72ee83c32877
Ran-E <rany01@gmail.com>
2023-02-01 12:16:29 +0200

    Back to normal

--- worlde-ran/src/components/the game/worlde.tsx
+++ worlde-ran/src/components/the game/worlde.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
+
 import Keyboard from "../../containers/keyboard/keyboard";
 import "./wordle.scss";
 
@@ -69,8 +70,7 @@ const Wordle = ({ word, numberOfLines, numberOfInputs }: Props) => {
     setColor(updatedColor);
 
     document
-      .getElementsByTagName("input")
-      [row * numberOfInputs + col + 1].focus();
+      .getElementsByTagName("input")[row * numberOfInputs + col + 1].focus();
   };
   const handleClick = (letter: string) => {
     const updatedInputs = inputs.map((inputRow, i) => {
@@ -114,8 +114,7 @@ const Wordle = ({ word, numberOfLines, numberOfInputs }: Props) => {
     setColor(updatedColor);
 
     document
-      .getElementsByTagName("input")
-      [currentRow * numberOfInputs + currentCol + 1].focus();
+      .getElementsByTagName("input")[currentRow * numberOfInputs + currentCol + 1].focus();
   };
 
   return (
